45c90a8a1e7fd504d38418ded0f18ba8575e8852
shibin <3developernt@gmail.com>
2026-05-09 15:32:15 +0530

    grade details

--- Autonoma_ERP/src/menu-items/erp.js
+++ Autonoma_ERP/src/menu-items/erp.js
@@ -66,17 +66,16 @@ export const masters = {
           url: '/master/hr/designation'
         },
         {
-<<<<<<< HEAD
           id: 'master-hr-grade-details',
           title: 'Grade Details',
           type: 'item',
           url: '/master/hr/grade'
-=======
+        },
+        {
           id: 'master-hr-designation-level',
           title: 'Designation Level',
-          type: 'item', 
+          type: 'item',
           url: '/master/hr/desg-level'
->>>>>>> eb345df02f3fff5159b2fe2b88cf7f4f7c98dae3
         }
       ]
     },
